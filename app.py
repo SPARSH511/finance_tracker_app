00184821973b94588e68b97c4b054ca6035b4ff7
Sparsh Saxena <90604939+SPARSH511@users.noreply.github.com>
2025-03-21 13:07:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -762,4 +762,4 @@ if __name__ == '__main__':
     
     # Start the Flask app
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=False,port=port)
+    app.run(debug=False,host="0.0.0.0",port=port)
